ef5b4edddea3c449e68e8dad419f86c85e1d1ab6
João Marcus <joao.marcus@a3data.com.br>
2023-08-29 20:05:08 -0300

    wip

--- test/mocks/infra/repositories/dynamodb/customers/mock-dynamodb.ts
+++ test/mocks/infra/repositories/dynamodb/customers/mock-dynamodb.ts
@@ -4,6 +4,7 @@ export const mockCustomerDynamodb = () => ({
   last_name: 'Doe',
   email: 'johndoe@gmail.com',
   phone: '5583999351425',
+  password: '123',
 });
 
 export const mockDynamodbList = () => ({
